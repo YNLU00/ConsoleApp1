f9e4545340d9a5f00afe5e200d35bc3b84a7eac5
ynlu@simcorp.com <ynlu@simcorp.com>
2022-02-21 12:11:52 +0100

    commit2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             //line1
-            Console.WriteLine("Hello world123!");
+            Console.WriteLine("Hello world123$$!");
             // Hello world
         }
     }
